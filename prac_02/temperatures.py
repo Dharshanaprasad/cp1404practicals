05c1f0e9bddfc98d0bf0debdf5dbaff99d42d843
Dharshanaprasad <dharshanasimran.prasad@myjcu.edu.au>
2025-06-01 22:17:36 +1000

    new temperature file

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,23 +1,18 @@
-# temperatures.py
-# Temperature conversion tool
+# This program converts temperatures between Celsius and Fahrenheit
 
 def main():
-    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
-    print(MENU)
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Enter temperature in Celsius: "))
-            fahrenheit = celsius * 9 / 5 + 32
-            print(f"Equivalent in Fahrenheit: {fahrenheit:.2f} F")
-        elif choice == "F":
-            fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-            celsius = (fahrenheit - 32) * 5 / 9
-            print(f"Equivalent in Celsius: {celsius:.2f} C")
-        else:
-            print("Invalid selection. Try again.")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Goodbye.")
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius}°C = {fahrenheit:.2f}°F")
+
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit}°F = {celsius:.2f}°C")
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
 
 main()
